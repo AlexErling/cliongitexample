2a23cef41980cf8d27caaeac118c9099d7e6de9e
Alex Erling <alex.erling@gmail.com>
2018-10-31 14:40:57 -0700

    First commit

---
+++ cmake-build-debug/CMakeFiles/CLionGitExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLionGitExample.dir/main.cpp.o"
+  "CLionGitExample.pdb"
+  "CLionGitExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLionGitExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CLionGitExample.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CLionGitExample.
+# This may be replaced when dependencies are built.
